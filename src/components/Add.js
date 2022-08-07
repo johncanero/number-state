1bec0f5faa6273c1dc9913c438407aab37eb5c45
John Canero <johncanero.official@gmail.com>
2022-08-07 11:27:15 +0800

    Modify and Add Comments

--- src/components/Add.js
+++ src/components/Add.js
@@ -3,19 +3,21 @@ import React, { useState } from 'react';
 
 const Add = () => {
 
+  // Declare a new state variable, which we'll call "count"
   const [count, setCount] = useState(0);
 
-
+  // Set the Array Count to Increase
   const increase = () => {
     setCount(count + 1)
   }
 
+  // Set the Array Count to Decrease
   const decrease = () => {
     setCount(count - 1)
   }
 
   
-
+  // Set the Starter "useState(0) - Define the const in order to work the Function
   return (
     <div className="container">
         <h1>{count}</h1>
